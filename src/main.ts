bcc583891b25eee233219d18a43ecee08211329e
smicle <bhhs1270026@gn.iwasaki.ac.jp>
2020-01-18 00:59:38 +0900

    setup vuetify

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import '@mdi/font/css/materialdesignicons.css'
+import App from '@/App.vue'
+import router from '@/plugins/router'
+import vuetify from '@/plugins/vuetify'
+// import '@/plugins/firebase'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  vuetify,
+  render: h => h(App),
+}).$mount('#app')
