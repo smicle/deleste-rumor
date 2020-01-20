a8833f49445526da4cb55d30f61ce5717f07037b
smicle <smicle@pepabo.com>
2020-01-20 18:48:02 +0900

    styleの設定を別コンポーネントに分ける

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,15 +3,15 @@
     <h1>デレステウワサクイズ</h1>
 
     <v-row>
-      <v-col cols="8" style="padding-right: 0">
-        <IdolCard style="margin-right: 0; min-height: 92px">
+      <v-col cols="8" :style="style.pr_0">
+        <IdolCard :style="style.mr_0 + style.H_92">
           <template v-slot:title>名前</template>
           <template v-slot:text>{{ flag ? '' : idol.name }}</template>
         </IdolCard>
       </v-col>
 
-      <v-col cols="4" style="padding-left: 0">
-        <img :src="src" :style="`height: 92px; ${flag ? 'opacity: 0' : ''}`" />
+      <v-col cols="4" :style="style.pl_0">
+        <img :src="src" :style="style.H_92 + (flag ? style.op_0 : '')" />
       </v-col>
     </v-row>
 
@@ -25,7 +25,7 @@
 </template>
 
 <script lang="ts">
-import {Component, Emit, Prop, Vue} from 'vue-property-decorator'
+import {Component, Emit, Prop, Vue, Mixins} from 'vue-property-decorator'
 import IdolCard from '@/components/IdolCard.vue'
 import NextBtn from '@/components/NextBtn.vue'
 import rumors from '@/json/scraping-rumor.json'
@@ -36,19 +36,30 @@ interface Idol {
   text: string
 }
 
+@Component
+export class MixinStyle extends Vue {
+  private style = {
+    mr_0: 'margin-right: 0;',
+    pr_0: 'padding-right: 0;',
+    pl_0: 'padding-left: 0;',
+    H_92: 'height: 92px;',
+    op_0: 'opacity: 0;',
+  }
+}
+
 @Component({
   components: {
     IdolCard,
     NextBtn,
   },
 })
-export default class Home extends Vue {
+export default class Home extends Mixins(MixinStyle) {
   private flag: boolean = true
   private idol: Idol = {name: '', text: ''}
   private rand: number = 0
   private src: string = ''
 
-  public async toggle() {
+  private async toggle() {
     this.flag = !this.flag
     if (this.flag) {
       this.rand = await randomNumber(0, rumors.length)
