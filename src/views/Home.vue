0c02e64e2404a540d65ddeb230e7b4491863e86c
smicle <smicle@pepabo.com>
2020-01-20 17:59:29 +0900

    Add IdolCard.vue NextBtn.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,50 +1,61 @@
 <template>
-  <v-container class="home" grid-list-sm>
+  <v-container class="home">
     <h1>デレステウワサクイズ</h1>
+
     <v-row>
-      <v-col cols="8" style="margin-right: 0; padding-right: 0">
-        <v-card class="card-name">
-          <v-card-title class="body-2 red-text">名前</v-card-title>
-          <v-card-text class="body-1" style="color: rgba(0,0,0,.87)">
-            {{ bool ? '' : name }}
-          </v-card-text>
-        </v-card>
+      <v-col cols="8" style="padding-right: 0">
+        <IdolCard style="margin-right: 0; min-height: 92px">
+          <template v-slot:title>名前</template>
+          <template v-slot:text>{{ flag ? '' : idol.name }}</template>
+        </IdolCard>
       </v-col>
-      <v-col cols="4" style="margin-left: 0; padding-left: 0">
-        <img
-          :class="`idol ${bool ? 'opacity-0' : ''}`"
-          :src="require(`@/assets/img/${name}.png`)"
-        />
+
+      <v-col cols="4" style="padding-left: 0">
+        <img :src="src" :style="`height: 92px; ${flag ? 'opacity: 0' : ''}`" />
       </v-col>
     </v-row>
-    <v-card class="card-rumor">
-      <v-card-title class="body-2">ウワサ</v-card-title>
-      <v-card-text class="body-1" style="color: rgba(0,0,0,.87)">{{ text }}</v-card-text>
-    </v-card>
-    <v-btn @click="toggle" :color="bool ? 'warning' : 'info'">
-      {{ bool ? '結果' : '次へ' }}
-    </v-btn>
+
+    <IdolCard>
+      <template v-slot:title>ウワサ</template>
+      <template v-slot:text>{{ idol.text }}</template>
+    </IdolCard>
+
+    <NextBtn @click="toggle" :flag="flag"></NextBtn>
   </v-container>
 </template>
 
 <script lang="ts">
 import {Component, Emit, Prop, Vue} from 'vue-property-decorator'
-import rumor from '@/json/scraping-rumor.json'
+import IdolCard from '@/components/IdolCard.vue'
+import NextBtn from '@/components/NextBtn.vue'
+import rumors from '@/json/scraping-rumor.json'
 const randomNumber = require('random-number-csprng')
 
-@Component
+interface Idol {
+  name: string
+  text: string
+}
+
+@Component({
+  components: {
+    IdolCard,
+    NextBtn,
+  },
+})
 export default class Home extends Vue {
-  public bool: boolean = true
-  public name: string = ''
-  public text: string = ''
-  public rand: number = 0
+  private flag: boolean = true
+  private idol: Idol = {name: '', text: ''}
+  private rand: number = 0
+  private src: string = ''
 
   public async toggle() {
-    this.bool = !this.bool
-    if (this.bool) {
-      this.rand = await randomNumber(0, rumor.length)
-      this.text = rumor[this.rand].text
-      this.name = rumor[this.rand].name
+    this.flag = !this.flag
+    if (this.flag) {
+      this.rand = await randomNumber(0, rumors.length)
+      const rumor = rumors[this.rand]
+
+      this.idol = {name: rumor.name, text: rumor.text}
+      this.src = require(`@/assets/img/${rumor.name}.png`)
     }
   }
 
@@ -52,21 +63,4 @@ export default class Home extends Vue {
 }
 </script>
 
-<style scoped>
-.card-name {
-  min-height: 92px;
-  margin: 15px;
-}
-.card-rumor {
-  margin: 15px;
-}
-.idol {
-  right: -50px;
-  margin-top: 12px;
-  padding: 0;
-  height: 92px;
-}
-.opacity-0 {
-  opacity: 0;
-}
-</style>
+<style scoped></style>
