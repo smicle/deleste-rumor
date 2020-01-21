49d0d6fe58025bfe272165fe0f01f94efc1c4e4e
smicle <smicle@pepabo.com>
2020-01-21 15:02:19 +0900

    Add ローディングが入るように変更

--- README.md
+++ README.md
@@ -20,3 +20,25 @@ yarn build
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### /src/plugins/firebase.ts
+
+```TypeScript
+import firebase from 'firebase/app'
+import 'firebase/app'
+import 'firebase/analytics'
+
+export const firebaseConfig = {
+  apiKey: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+  authDomain: 'XXXXX.firebaseapp.com',
+  databaseURL: 'https://XXXXXX.firebaseio.com',
+  projectId: 'XXXXXX',
+  storageBucket: 'XXXXXX.appspot.com',
+  messagingSenderId: 'XXXXXXXXXXXXX',
+  appId: '1:XXXXXXXXXXXXX:web:XXXXXXXXXXXXXXXXXXXXXX',
+  measurementId: 'X-XXXXXXXXXX',
+}
+
+firebase.initializeApp(firebaseConfig)
+firebase.analytics()
+```
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,15 +3,15 @@
     <h1>デレステウワサクイズ</h1>
 
     <v-row>
-      <v-col cols="8" :style="style.pr_0">
-        <IdolCard :style="style.mr_0 + style.H_92">
+      <v-col cols="8" class="pr-0">
+        <IdolCard class="mr-0" :style="H_92">
           <template v-slot:title>名前</template>
           <template v-slot:text>{{ flag ? '' : idol.name }}</template>
         </IdolCard>
       </v-col>
 
-      <v-col cols="4" :style="style.pl_0">
-        <img :src="src" :style="style.H_92 + (flag ? style.op_0 : '')" />
+      <v-col cols="4" class="pl-0">
+        <img :src="idol.src" :style="H_92 + (flag ? op_0 : '')" />
       </v-col>
     </v-row>
 
@@ -20,7 +20,7 @@
       <template v-slot:text>{{ idol.text }}</template>
     </IdolCard>
 
-    <NextBtn @click="toggle" :flag="flag"></NextBtn>
+    <NextBtn @click="toggle" :flag="flag" :promise="promise"></NextBtn>
   </v-container>
 </template>
 
@@ -34,17 +34,13 @@ const randomNumber = require('random-number-csprng')
 interface Idol {
   name: string
   text: string
+  src: string
 }
 
 @Component
 export class MixinStyle extends Vue {
-  private style = {
-    mr_0: 'margin-right: 0;',
-    pr_0: 'padding-right: 0;',
-    pl_0: 'padding-left: 0;',
-    H_92: 'height: 92px;',
-    op_0: 'opacity: 0;',
-  }
+  private H_92 = 'height: 92px;'
+  private op_0 = 'opacity: 0;'
 }
 
 @Component({
@@ -54,20 +50,26 @@ export class MixinStyle extends Vue {
   },
 })
 export default class Home extends Mixins(MixinStyle) {
+  private promise: boolean = false
   private flag: boolean = true
-  private idol: Idol = {name: '', text: ''}
   private rand: number = 0
-  private src: string = ''
+  private idol: Idol = {name: '', text: '', src: ''}
 
   private async toggle() {
     this.flag = !this.flag
-    if (this.flag) {
-      this.rand = await randomNumber(0, rumors.length)
-      const rumor = rumors[this.rand]
+    if (!this.flag) return
 
-      this.idol = {name: rumor.name, text: rumor.text}
-      this.src = require(`@/assets/img/${rumor.name}.png`)
+    this.rand = await randomNumber(0, rumors.length)
+    const rumor = rumors[this.rand]
+
+    this.idol = {
+      name: rumor.name,
+      text: rumor.text,
+      src: require(`@/assets/img/${rumor.name}.png`),
     }
+
+    this.promise = true
+    fetch(this.idol.src).then(_ => (this.promise = false))
   }
 
   onload = this.toggle()
